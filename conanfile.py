62f2cf7b2b6f540a1f55f8c753f304fa12eba082
Manu343726 <Manu343726@gmail.com>
2026-07-08 22:17:22 +0200

    migrate deps from FetchContent to conan (glm, yaml-cpp, glfw, imgui); add conanfile.py and gcc profile; full toolchain via conan tool_requires

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+
+class SyclSandboxConan(ConanFile):
+    name = "sycl-sandbox"
+    version = "0.1.0"
+
+    settings = "os", "compiler", "build_type", "arch"
+
+    requires = [
+        "glm/1.0.1",
+        "yaml-cpp/0.8.0",
+        "glfw/3.4",
+        "imgui/1.92.8",
+    ]
+
+    tool_requires = [
+        "cmake/3.31.12",
+        "ninja/1.12.1",
+    ]
+
+    generators = "CMakeToolchain", "CMakeDeps"
