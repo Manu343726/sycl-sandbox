4a35a46f57b0bca38e227f2c8f709fe651825e4c
Manu343726 <Manu343726@gmail.com>
2026-07-10 21:07:19 +0200

    Use SFINAE for push_back host-only constraint instead of static_assert

--- include/containers/vector.h
+++ include/containers/vector.h
@@ -69,9 +69,9 @@ public:
 
 
 
+    template <bool Enable = (Tag == alloc::Target::Host),
+              typename = std::enable_if_t<Enable>>
     void push_back(const void *element) {
-        static_assert(Tag == alloc::Target::Host,
-                      "push_back requires a host vector; build on host then transfer() to device");
         auto buf = allocator_.allocate(element_size_, alignment_);
         if (!buf.is_valid()) return;
         alloc::raw::memcpy<Tag>(buf, element, element_size_, *queue_);
@@ -128,9 +128,9 @@ public:
     vector(const vector &) = default;
     vector &operator=(const vector &) = default;
 
+    template <bool Enable = (Tag == alloc::Target::Host),
+              typename = std::enable_if_t<Enable>>
     void push_back(const T &element) {
-        static_assert(Tag == alloc::Target::Host,
-                      "push_back requires a host vector; build on host then transfer() to device");
         impl_.push_back(&element);
     }
 
